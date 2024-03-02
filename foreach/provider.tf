ca783dde474eed6f346be337c436ca57fdc1effe
disha <disha@123gmaile.com>
2024-03-02 23:39:38 +0530

    t

--- foreach/provider.tf
+++ foreach/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "5.36.0"
     }
   }
+
+  backend "s3" {
+    bucket = "r-remote-state"
+    key = "for_each-demo"
+    region = "us-east-1"
+    dynamodb_table = "roboshop-locking"
+  }
 }
 
 provider "aws" {
